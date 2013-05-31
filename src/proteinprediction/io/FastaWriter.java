3e4ccbcb0baee0f40ecfb73113cab68248e1d726
quant42 <yann_spoeri@web.de>
2013-05-31 21:24:19 +0200

    FastaWriter

--- src/proteinprediction/io/FastaWriter.java
+++ src/proteinprediction/io/FastaWriter.java
@@ -7,10 +7,40 @@ import java.io.*;
  * @author Yann
  */
 public class FastaWriter {
+
+    /**
+     * stream to safe everything in
+     */
+    private BufferedWriter bf;
     
     /**
-     * The file to safe everything in
+     * constuctor for a fasta filewriter
      */
-    public File output;
+    public FastaWriter(File f) throws IOException {
+        this.bf = new BufferedWriter(new FileWriter(f));
+    }
     
+    /**
+     * write data to output file
+     */
+    public void write(String fastaHeader, String[] fastaLines) throws IOException {
+        bf.write(">" + fastaHeader + "\n");
+        for(int i = 0; i < fastaLines.length; i++) {
+            bf.write(fastaLines[i] + "\n");
+        }
+    }
+
+    /**
+     * close the stream
+     */
+    public void close() throws IOException {
+        bf.close();
+    }
+    
+    /**
+     * flush the stream
+     */
+    public void flush() throws IOException {
+        bf.close();
+    }
 }
