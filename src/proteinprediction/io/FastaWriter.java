443ab43611604fd79d3830ad236795c05ce317ed
quant42 <yann_spoeri@web.de>
2013-05-31 21:06:10 +0200

    fastaWriter

---
+++ src/proteinprediction/io/FastaWriter.java
@@ -0,0 +1,16 @@
+package proteinprediction.io;
+
+import java.io.*;
+
+/**
+ *
+ * @author Yann
+ */
+public class FastaWriter {
+    
+    /**
+     * The file to safe everything in
+     */
+    public File output;
+    
+}
