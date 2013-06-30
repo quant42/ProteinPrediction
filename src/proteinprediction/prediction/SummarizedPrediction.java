aceca494f217bb74f4dcf7e5472ba9cc6fd54243
quant42 <yann_spoeri@web.de>
2013-06-30 08:30:32 +0200

    stuff

--- src/proteinprediction/prediction/SummarizedPrediction.java
+++ src/proteinprediction/prediction/SummarizedPrediction.java
@@ -5,5 +5,26 @@ package proteinprediction.prediction;
  * @author spoeri
  */
 public class SummarizedPrediction {
+     public String seq;
+     boolean isProtein;
+     double isProtConv;
+     String inMembran;
+     String inMembranConv;
+     String insideOutside;
+     String insideOutsideConv;
+     String tmhTml;
+     String tmhtmlConv;
      
+     public SummarizedPrediction() {}
+     public SummarizedPrediction(String seq, boolean isProtein, double isProtConv, String inMembran, String inMembranConv, String insideOutside, String insideOutsideConv, String tmhTml, String tmhtmlConv) {
+         this.seq = seq;
+         this.isProtein = isProtein;
+         this.isProtConv = isProtConv;
+         this.inMembran = inMembran;
+         this.inMembranConv = inMembranConv;
+         this.insideOutside = insideOutside;
+         this.insideOutsideConv = insideOutsideConv;
+         this.tmhTml = tmhTml;
+         this.tmhtmlConv = tmhtmlConv;
+     }
 }
